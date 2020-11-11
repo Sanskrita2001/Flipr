843f9fe39a82adf36bc9a61fdcf5bb7b35fbd247
Sanskrita Saha <sanskrita2001@gmail.com>
2020-11-11 15:48:26 +0530

    Updates

---
+++ precache-manifest.6928df151aac0b6aaedec5d1f3977fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20d83dc0ea68040acce13152401dcb9a",
+    "url": "/Flipr/index.html"
+  },
+  {
+    "revision": "945ca9394c932d791738",
+    "url": "/Flipr/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eeb2e1abd51f69042bc7",
+    "url": "/Flipr/static/js/2.0767997b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Flipr/static/js/2.0767997b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "945ca9394c932d791738",
+    "url": "/Flipr/static/js/main.acf38889.chunk.js"
+  },
+  {
+    "revision": "616010da8b5f5f87e14a",
+    "url": "/Flipr/static/js/runtime-main.3d8c2ec1.js"
+  }
+]);
